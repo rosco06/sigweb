01e26da2927aabe8b9fa06c925b869e6542e81f5
rosco06 <65193481+rosco06@users.noreply.github.com>
2020-10-07 10:49:44 -0500

    ejemplo_clase
    Conociendo como subir archivos a github de Qgis2web

---
+++ data/JAL_1.js
@@ -0,0 +1,8 @@
+var json_JAL_1 = {
+"type": "FeatureCollection",
+"name": "JAL_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OID_": 0.0, "Name": "Cra. 71 #2C-21", "FolderPath": "JAL", "SymbolID": 0.0, "AltMode": -1.0, "Base": 0.0, "Snippet": "Cra. 71 #2C-21, Cali, Valle del Cauca", "PopupInfo": "<!DOCTYPE html><html><head><\/head><body><script type=\"text\/javascript\">window.location.href=\"https:\/\/www.google.com\/earth\/rpc\/entity?lat=3.3930325000000003&lng=-76.5503039&fid=0x8e30a3e2ed2f2469:0xb988972d6c5ac128&hl=es-419&gl=co&client=earth-client&cv=7", "HasLabel": -1.0, "LabelID": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.5503039, 3.393032500000061 ] } }
+]
+}
---
+++ data/Saludp_2.js
@@ -0,0 +1,17 @@
+var json_Saludp_2 = {
+"type": "FeatureCollection",
+"name": "Saludp_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.545320727007237, 3.387282638765614 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.557737598503067, 3.386930321016107 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.551615895868252, 3.39190932130895 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.544522970775631, 3.375926117806168 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.545639637772922, 3.375792074792118 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.555914644536813, 3.375404166584703 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.55420328534592, 3.390655209735889 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.549459869178818, 3.388390810338424 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.558647887931585, 3.382955211582348 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -76.545417955398946, 3.38255067067348 ] } }
+]
+}
